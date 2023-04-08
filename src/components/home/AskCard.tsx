19e8a5b8d04167675e660e209cb991cc2fa931ad
MridulSheikh <prince9nazir@gmail.com>
2023-04-08 22:05:17 +0600

    add animation

--- src/components/home/AskCard.tsx
+++ src/components/home/AskCard.tsx
@@ -1,45 +1,60 @@
-import React, { useState } from 'react'
-import {motion} from 'framer-motion'
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-interface props{
-    title: string;
-    body: string;
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+interface props {
+  title: string;
+  body: string;
 }
-function AskCard({title, body}:props) {
-    const [clicked, setClicked] = useState(false)
 
-    const paraVariants = {
-        hidden : {
-            opacity : 0,
-        },
-        visible : {
-            opacity : 1,
-            transition : {
-                duration : 0.70
-            }
-        }
+const paraVariants = {
+  hidden: {
+    height: 0,
+  },
+  visible: {
+    height: "auto",
+    transition: {
+      duration: 0.7,
     }
+  },
+};
 
-    return (
-        <div onClick={() => setClicked(!clicked)}  className={`p-5  mt-5 ${!clicked && 'bg-white shadow-md' } ease-in duration-200 rounded-md`}>
-            <div className='text-lg  font-bold flex justify-between'>
-                <p>{title}</p>
-                <motion.button initial={{rotate: -250}} animate={{rotate: 0}} transition={{duration : 0.50, delay: 0.70}}>
-                    {
-                        !clicked ? 
-                        <AiOutlinePlus className='font-bold text-primarymain text-xl' />
-                        :
-                        <AiOutlineMinus className='font-bold text-primarymain text-xl' />
-                    }
-                </motion.button>
-            </div>
-            {
-                clicked &&
-                <motion.div variants={paraVariants} initial="hidden" animate="visible" className='mt-6'>
-                    {body}
-                </motion.div>
-            }
-        </div>
-    )
-        }
+function AskCard({ title, body }: props) {
+  const [clicked, setClicked] = useState(false);
+  return (
+    <div
+      onClick={() => setClicked(!clicked)}
+      className={`p-10  mt-5 bg-white shadow-md overflow-hidden ease-in duration-200 rounded-md`}
+    >
+      <div className="md:text-2xl font-semibold flex justify-between">
+        <p className="md:text-lg">{title}</p>
+        <motion.button
+          initial={{ rotate: -250 }}
+          animate={{ rotate: 0 }}
+          transition={{ duration: 0.5, delay: 0.7 }}
+          className="transition-all duration-500"
+        >
+          {!clicked ? (
+            <AiOutlinePlus className="font-bold text-primarymain text-xl" />
+          ) : (
+            <AiOutlineMinus className="font-bold text-primarymain text-xl" />
+          )}
+        </motion.button>
+      </div>
+      {/* @ts-ignore */}
+      <AnimatePresence>
+        {clicked && (
+          <motion.div
+            variants={paraVariants}
+            initial="hidden"
+            animate="visible"
+            exit={{height : 0}}
+            className={`mt-6`}
+          >
+            {body}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
 export default AskCard;
--- src/components/home/FequendlyAskQuistion.tsx
+++ src/components/home/FequendlyAskQuistion.tsx
@@ -1,22 +1,55 @@
 import { faq } from '../../fakedata/FAQ'
 import AskCard from './AskCard'
+import {motion} from 'framer-motion'
+
+const parentVarients = {
+    hidden : {
+        x: -250,
+        opacity : 0,
+      },
+      visible : {
+        x: 0,
+        opacity : 1,
+        transition: {
+            type: "spring",
+            mass: 0.4,
+            damping: 8,
+            when: "beforeChildren",
+            staggerChildren: 0.4,
+          },
+      }
+}
+const imgVarients = {
+    hidden : {
+        x: 250,
+        opacity : 0,
+      },
+      visible : {
+        x: 0,
+        y: [0, -20],
+        opacity : 1,
+        transition: {
+            duration : 1,
+            delay : 0.70,
+            y: {
+                yoyo : Infinity,
+            }
+          },
+      }
+}
 
 function FequendlyAskQuistion() {
-   
     return (
         <div className='container pt-10 px-5 xl:px-0 xl:max-w-screen-lg mx-auto'>
             <div className='text-center text-3xl font-bold mt-10'>
                 Frequently Asked <span className='text-primarymain'>Questions</span>
             </div>
-            <div className='grid md:grid-cols-5 gap-6 mt-20'>
-                <div className='col-span-3'>
+            <div className='mt-20'>
+                <motion.div variants={parentVarients} initial="hidden" animate="visible" className='col-span-3'>
                     {
                         faq.map(fq => <AskCard key={fq.title} title={fq.title} body={fq.body} /> )
                     }
-                </div>
-                <div className='col-span-2 relative'>
-                    <img src="/img/faq.png" className='w-full h-full object-contain my-auto' alt="faq" />
-                </div>
+                </motion.div>
             </div>
         </div>
     )
